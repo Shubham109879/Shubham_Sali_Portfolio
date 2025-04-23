00636bb1c25cf53bd3ecbe32fdc7339c7b397e4b
Shubham109879 <salishubham14@gmail.com>
2025-04-23 12:16:26 +0530

    shubham_portfolio_changes_2025_04_23

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,10 @@ function headerShadow() {
    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
    navHeader.style.height = "70px";
    navHeader.style.lineHeight = "70px"; 
+   navHeader.style.margin="10px";
+   navHeader.style.borderRadius="50px";
+   navHeader.style.width="98.8%";
+   navHeader.style.backgroundColor="rgba(0, 0, 0, 0.05)";
    
 }
 
